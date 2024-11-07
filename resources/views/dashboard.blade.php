912726f8ede9a3ec726686e5e82173d2a23309ac
hatsune.saito <hatsudon@gmail.com>
2024-11-07 02:47:10 +0000

    トップページの写真レイアウト変更

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,12 +10,15 @@
         <ul class="mt-3 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 md:gap-6 xl:gap-8">
             @foreach ($photos as $photo)
                 <li class="group h-64 flex justify-end items-end bg-gray-100 overflow-hidden rounded-lg shadow-lg relative">
-                        <a href="{{ $photo->photo_url }}">
+                        <a href="{{ route('photos.show', $photo->id) }}">
                         <img
                             src="{{ $photo->photo_url }}"
                             class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-105 transition duration-200"
                         />
                         </a>
+                        <div class="absolute inset-x-0 bottom-0 h-1/6 bg-gradient-to-b from-transparent to-black">
+                            <p class="text-white absolute right-4 bottom-2">{{ $photo->created_at }}</p>
+                        </div>
                 </li>
             @endforeach
         </ul>
